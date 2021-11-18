ffd7470e7ab3e144893862f8bf309f0e1e74b056
Coprine <89653731+Coprine@users.noreply.github.com>
2021-11-18 12:51:22 -0500

    Add files via upload

---
+++ lab1.js
@@ -0,0 +1,12 @@
+var secondsInAMinute = 60;
+var minutesInAHour = 60;
+var hoursInADay = 24;
+var daysInAYear = 365;
+var secondsInAnHour = secondsInAMinute * minutesInAHour;
+var secondsInADay = secondsInAnHour * hoursInADay;
+var secondsInAYear = secondsInADay * daysInAYear;
+
+document.write("The number of seconds in a year is " + secondsInAYear + ".");
+document.write("<br>");
+document.write("<br>");
+document.write("Project by Patrick M. Cook");
---
+++ lab1_2.js
@@ -0,0 +1,25 @@
+var apples = 10;
+
+document.write("The number of apples I have is " + apples + "<br>");
+apples++;
+document.write("The number of apples I have is " + apples + "<br>");
+apples++;
+document.write("The number of apples I have is " + apples + "<br>");
+apples++;
+document.write("The number of apples I have is " + apples + "<br>");
+apples++;
+document.write("The number of apples I have is " + apples + "<br>");
+apples++;
+document.write("The number of apples I have is " + apples + "<br>");
+
+document.write("<br><br>");
+
+var ninjas = 100;
+
+document.write("There are " + --ninjas + " ninjas in the room");
+document.write("There are " + --ninjas + " ninjas in the room");
+document.write("There are " + --ninjas + " ninjas in the room");
+document.write("There are " + --ninjas + " ninjas in the room");
+document.write("There are " + --ninjas + " ninjas in the room");
+
+document.write("Project by Patrick M. Cook");
